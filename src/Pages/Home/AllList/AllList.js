d9ae4c64ba6333a595fd9b20fc01d30c126d1cd8
masum691 <masumhu691@gmail.com>
2022-01-20 16:57:26 +0600

    delete list

--- src/Pages/Home/AllList/AllList.js
+++ src/Pages/Home/AllList/AllList.js
@@ -1,1 +1,14 @@
-rsc
+// import React from 'react';
+
+// const AllList = ({list}) => {
+//     const {name, email} = list;
+//     return (
+//         <div>
+//             <h5>{name}</h5>
+//             <h6>{email}</h6>
+//             <hr />
+//         </div>
+//     );
+// };
+
+// export default AllList;
--- src/Pages/Home/AllLists/AllLists.js
+++ src/Pages/Home/AllLists/AllLists.js
@@ -7,12 +7,31 @@ const AllLists = () => {
         .then(res => res.json())
         .then(data => setLists(data))
     }, [])
+
+    // delete an user
+    const handleDeleteList = id => {
+        const proceed = window.confirm('Are you sure want to delete?')
+        if(proceed){
+            fetch(`http://localhost:5000/mytodo/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                alert('deleted successfully')
+                const remainList = lists.filter(lists => lists._id !== id)
+                setLists(remainList)
+            }
+        })
+        }
+    }
+
     return (
         <div>
             <h1>All lists {lists.length}</h1>
             <div className='bg-primary'>
             {
-                lists.map(list => <h5 className='bg-warning'>{list.name} {list.email}</h5>)
+                lists.map(list => <h5 className='bg-warning'>{list.name} {list.email} <button>update</button> <button onClick={() => handleDeleteList(list._id)}>delete</button></h5>)
             }
             </div>
         </div>
